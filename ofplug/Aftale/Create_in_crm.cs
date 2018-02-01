dc0fc40a385305a48ff42099d4fec4033b3c91b3
kkkSvend <svend.l@kirkenskorshaer.dk>
2018-02-01 12:28:51 +0100

    Adding hello world example

---
+++ ofplug/Aftale/Create_in_crm.cs
@@ -0,0 +1,31 @@
+﻿using System.Activities;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Workflow;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ofplug.Aftale
+{
+	public class Create_in_crm : CodeActivity
+	{
+		protected override void Execute(CodeActivityContext codeActivityContext)
+		{
+			//Create the tracing service
+			ITracingService tracingService = codeActivityContext.GetExtension<ITracingService>();
+
+			//Create the context
+			IWorkflowContext workflowContext = codeActivityContext.GetExtension<IWorkflowContext>();
+			IOrganizationServiceFactory serviceFactory = codeActivityContext.GetExtension<IOrganizationServiceFactory>();
+			IOrganizationService service = serviceFactory.CreateOrganizationService(workflowContext.UserId);
+
+			tracingService.Trace("ofplug_aftale_create: creating aftale");
+
+			Entity entity = new Entity("nrq_bidragsaftale");
+			entity["nrq_type"] = "hello world";
+			service.Create(entity);
+		}
+	}
+}
