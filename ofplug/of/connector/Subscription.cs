d3d56c05b95e60f2896e7cdca941b78721595441
kkkSvend <svend.l@kirkenskorshaer.dk>
2018-02-16 11:08:48 +0100

    adding connector for subscriptions

---
+++ ofplug/of/connector/Subscription.cs
@@ -0,0 +1,29 @@
+﻿namespace ofplug.of.connector
+{
+	public class Subscription : Abstract_data_exchange
+	{
+		public Subscription(Sender sender, string url, string path) : base(sender, url, path)
+		{
+		}
+
+		public data.Subscription Get(int id)
+		{
+			return Get<data.Subscription>(id);
+		}
+
+		public data.IdResponse Patch(int id, data.Subscription of_subscription)
+		{
+			return base.Patch(id, of_subscription);
+		}
+
+		public data.IdResponse Put(int id, data.Subscription of_subscription)
+		{
+			return base.Put(id, of_subscription);
+		}
+
+		public data.IdResponse Post(data.Subscription of_subscription)
+		{
+			return base.Post(of_subscription);
+		}
+	}
+}
---
+++ ofplug/of/connector/Subscriptions.cs
@@ -0,0 +1,9 @@
+﻿namespace ofplug.of.connector
+{
+	public class Subscriptions : Abstract_id_collection
+	{
+		public Subscriptions(string url, int step) : base(url, "subscriptions", step)
+		{
+		}
+	}
+}
