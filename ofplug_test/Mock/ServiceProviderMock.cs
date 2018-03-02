fe01ecaffea782969addc2e1d8417f44d6944d25
kkkSvend <svend.l@kirkenskorshaer.dk>
2018-03-02 15:13:22 +0100

    adding mocks for plugin testing

---
+++ ofplug_test/Mock/ServiceProviderMock.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ofplug_test.Mock
+{
+	public class ServiceProviderMock : IServiceProvider
+	{
+		public object Service;
+
+		public object GetService(Type serviceType)
+		{
+			return Service;
+		}
+	}
+}
