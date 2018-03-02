6a5c7b62c9eb21f5c21ed802d0159187f3eccdb6
kkkSvend <svend.l@kirkenskorshaer.dk>
2018-03-02 10:23:31 +0100

    SenderLogs log is now enum based instead of string based

---
+++ ofplug_test/Mock/SenderLog.cs
@@ -0,0 +1,16 @@
+﻿namespace ofplug_test.Mock
+{
+	public class SenderLog
+	{
+		public string Url;
+		public object Request;
+		public SenderMock.Operation Operation;
+
+		public SenderLog(SenderMock.Operation operation, string url, object request)
+		{
+			Operation = operation;
+			Url = url;
+			Request = request;
+		}
+	}
+}
--- ofplug_test/Mock/SenderMock.cs
+++ ofplug_test/Mock/SenderMock.cs
@@ -5,36 +5,45 @@ namespace ofplug_test.Mock
 {
 	public class SenderMock : ISender
 	{
-		public List<KeyValuePair<string, object>> Log = new List<KeyValuePair<string, object>>();
+		public List<SenderLog> Log = new List<SenderLog>();
 		public Queue<object> data_to_return = new Queue<object>();
+
+		public enum Operation
+		{
+			Delete = 1,
+			Get = 2,
+			Patch = 3,
+			Post = 4,
+			Put = 5,
+		}
 
 		public Response Delete<Request, Response>(string url, Request request) where Response : class
 		{
-			Log.Add(new KeyValuePair<string, object>("Delete", request));
+			Log.Add(new SenderLog(Operation.Delete, url, request));
 			return data_to_return.Dequeue() as Response;
 		}
 
 		public Response Get<Response>(string url) where Response : class
 		{
-			Log.Add(new KeyValuePair<string, object>("Get", null));
+			Log.Add(new SenderLog(Operation.Get, url, null));
 			return data_to_return.Dequeue() as Response;
 		}
 
 		public Response Patch<Request, Response>(string url, Request request) where Response : class
 		{
-			Log.Add(new KeyValuePair<string, object>("Patch", request));
+			Log.Add(new SenderLog(Operation.Patch, url, request));
 			return data_to_return.Dequeue() as Response;
 		}
 
 		public Response Post<Request, Response>(string url, Request request) where Response : class
 		{
-			Log.Add(new KeyValuePair<string, object>("Patch", request));
+			Log.Add(new SenderLog(Operation.Post, url, request));
 			return data_to_return.Dequeue() as Response;
 		}
 
 		public Response Put<Request, Response>(string url, Request request) where Response : class
 		{
-			Log.Add(new KeyValuePair<string, object>("Patch", request));
+			Log.Add(new SenderLog(Operation.Put, url, request));
 			return data_to_return.Dequeue() as Response;
 		}
 	}
