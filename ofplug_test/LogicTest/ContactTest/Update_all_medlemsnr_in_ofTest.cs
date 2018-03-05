c6d6a1650e3254e482b4900c923a8526ae5cbfc6
kkkSvend <svend.l@kirkenskorshaer.dk>
2018-03-05 14:05:59 +0100

    adding test class for contact Update_all_medlemsnr_in_of

---
+++ ofplug_test/LogicTest/ContactTest/Update_all_medlemsnr_in_ofTest.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Activities;
+using System.Collections.Generic;
+
+namespace ofplug_test.LogicTest.ContactTest
+{
+	[TestClass]
+	public class Update_all_medlemsnr_in_ofTest : Abstract.AbstractTest
+	{
+		[TestMethod]
+		public void Medlemsnr_will_be_updated()
+		{
+			ofplug.Logic.Contact.Update_all_medlemsnr_in_of creator = Arrange_creator();
+			_sender.data_to_return.Enqueue(new List<int>() { _id.Get_id("contact_id_1") });
+			Add_of_contact();
+			Add_of_empty();
+			Add_crm_contact(contact =>
+			{
+				contact.new_ofcontactid = _id.Get_id("contact_id_1");
+				contact.new_kkadminmedlemsnr = _id.Get_id("medlemsnr").ToString();
+			});
+
+			WorkflowInvoker.Invoke(creator);
+
+			Assert_crm_operation(0, Mock.OrganizationServiceMock.Operation.RetrieveMultiple, "contact");
+			Assert_of_operation(0, Mock.SenderMock.Operation.Get, null);
+			Assert_of_operation(1, Mock.SenderMock.Operation.Get, null);
+			Assert_of_operation(2, Mock.SenderMock.Operation.Patch, typeof(ofplug.of.data.Contact));
+			Assert_number_of_operations(3, 1);
+		}
+
+		[TestMethod]
+		public void Medlemsnr_will_not_be_updated_if_it_is_already_up_to_date()
+		{
+			ofplug.Logic.Contact.Update_all_medlemsnr_in_of creator = Arrange_creator();
+			_sender.data_to_return.Enqueue(new List<int>() { _id.Get_id("contact_id_1") });
+			Add_of_contact(of_contact => of_contact.External_id = _id.Get_id("medlemsnr").ToString());
+			Add_of_empty();
+			Add_crm_contact(contact =>
+			{
+				contact.new_ofcontactid = _id.Get_id("contact_id_1");
+				contact.new_kkadminmedlemsnr = _id.Get_id("medlemsnr").ToString();
+			});
+
+			WorkflowInvoker.Invoke(creator);
+
+			Assert_crm_operation(0, Mock.OrganizationServiceMock.Operation.RetrieveMultiple, "contact");
+			Assert_of_operation(0, Mock.SenderMock.Operation.Get, null);
+			Assert_of_operation(0, Mock.SenderMock.Operation.Get, null);
+			Assert_number_of_operations(2, 1);
+		}
+
+		[TestMethod]
+		public void If_no_crm_contact_can_be_found_nothing_happens()
+		{
+			ofplug.Logic.Contact.Update_all_medlemsnr_in_of creator = Arrange_creator();
+			_sender.data_to_return.Enqueue(new List<int>() { _id.Get_id("contact_id_1") });
+			Add_of_contact();
+			_sender.data_to_return.Enqueue(new List<int>() { });
+			Add_crm_empty();
+
+			WorkflowInvoker.Invoke(creator);
+
+			Assert_crm_operation(0, Mock.OrganizationServiceMock.Operation.RetrieveMultiple, "contact");
+			Assert_of_operation(0, Mock.SenderMock.Operation.Get, null);
+			Assert_of_operation(0, Mock.SenderMock.Operation.Get, null);
+			Assert_of_operation(0, Mock.SenderMock.Operation.Get, null);
+			Assert_number_of_operations(3, 1);
+		}
+
+		private ofplug.Logic.Contact.Update_all_medlemsnr_in_of Arrange_creator()
+		{
+			ofplug.Logic.Contact.Update_all_medlemsnr_in_of creator = new ofplug.Logic.Contact.Update_all_medlemsnr_in_of()
+			{
+				IsTest = true
+			};
+
+			creator.Set_test(_tracingService, _service, _sender);
+
+			return creator;
+		}
+	}
+}
