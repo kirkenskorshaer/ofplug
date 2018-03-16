aa6bc0d28ed95337c4998a5ebc9573b732b2fa3c
kkkSvend <svend.l@kirkenskorshaer.dk>
2018-03-16 10:37:27 +0100

    adding of data InitiateAgreementResponse

---
+++ ofplug/of/data/InitiateAgreementResponse.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.Serialization;
+
+namespace ofplug.of.data
+{
+	[DataContract]
+	public class InitiateAgreementResponse
+	{
+		[DataMember(Name = "agreement_id", EmitDefaultValue = false)]
+		public int Agreement_id { get; set; }
+
+		[DataMember(Name = "subscription_id", EmitDefaultValue = false)]
+		public int Subscription_id { get; set; }
+
+		[DataMember(Name = "contact_id", EmitDefaultValue = false)]
+		public int Contact_id { get; set; }
+	}
+}
